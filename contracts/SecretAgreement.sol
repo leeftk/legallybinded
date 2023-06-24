911e6b78df3f43e26d582e87419d162350d04be2
Leandro Faria <lee.lara1219@gmail.com>
2023-06-23 21:06:10 -0400

    update tests

--- contracts/SecretAgreement.sol
+++ contracts/SecretAgreement.sol
@@ -11,12 +11,12 @@ error InvalidSecret();
 
 
 contract SecretAgreement {
+    
     using ECDSA for bytes32;
 
     struct Agreement {
         address party1;
         address party2;
-        bool isSecretRevealed;
         bool isParty1Voted;
         bool isParty2Voted;
     }
@@ -28,6 +28,7 @@ contract SecretAgreement {
     /// @dev Creates a new agreement with the provided secret hash and the second party's address.
     /// @param _secretHash The hash of the secret agreed upon by the parties.
     /// @param _party2 The address of the second party involved in the agreement.
+
     function createAgreement(bytes32 _secretHash, address _party2) external {
         Agreement storage agreement = agreements[_secretHash];
         require(agreement.party1 == address(0), "Agreement already exists for the secret hash.");
@@ -37,6 +38,7 @@ contract SecretAgreement {
         agreement.isParty2Voted = false;
         agreements[_secretHash] = agreement;
     }
+
     function agreeToSecret(bytes32 _secretHash) external {
         if(msg.sender != agreements[_secretHash].party1 && msg.sender != agreements[_secretHash].party2) revert YouAreNotPartOfParty();
         if(msg.sender == agreements[_secretHash].party1){
@@ -51,9 +53,11 @@ contract SecretAgreement {
     /// @param _secretHash The secret to be revealed.
     /// @param _secretToReveal The secret to be revealed.
     /// @param _signature The signature of the message hash to verify the authenticity of the revealer.
+
     function revealSecret(bytes32 _secretHash, string memory _secretToReveal, bytes memory _signature) external {
         bytes32 secretSig = _secretHash.toEthSignedMessageHash();
         bytes32 secretHash = keccak256(abi.encodePacked(_secretToReveal));
+
         if(msg.sender != agreements[_secretHash].party1 && msg.sender != agreements[_secretHash].party2) revert YouAreNotPartOfParty();
         require(agreements[_secretHash].isParty1Voted == true && agreements[_secretHash].isParty2Voted == true, "Both parties have not agreed to the secret");
         require(_verifySignature(secretSig, _signature), "Invalid signature.");
@@ -69,6 +73,7 @@ contract SecretAgreement {
     /// @param _messageHash The hash of the message that was signed.
     /// @param _signature The signature to be verified.
     /// @return A boolean indicating whether the signature is valid or not.
+
     function _verifySignature(bytes32 _messageHash, bytes memory _signature) internal view returns (bool) {
         address recoveredSigner = _messageHash.recover(_signature);
         return recoveredSigner == msg.sender;
--- test/SecretAgreement.js
+++ test/SecretAgreement.js
@@ -14,75 +14,228 @@ describe("SecretAgreement", function () {
     await secretAgreement.waitForDeployment();
   });
 
-  describe("Create Agreement", function () {
+  describe("Deployment", function () {
+    it("Should get address of deployed contract", async function () {
+      expect(await secretAgreement.getAddress()).to.not.equal(0);
+    });
 
-  it("should allow two parties to create an agreement and reveal the secret", async function () {
-    const secretHash = ethers.keccak256(ethers.toUtf8Bytes("Secret Message"));
-    // Party 1 creates the agreement
-    await secretAgreement.connect(signer1).createAgreement(secretHash, signer2.address);
-
-    // Party 1 & 2 agrees to the secret
-    await secretAgreement.connect(signer1).agreeToSecret(secretHash);
-    await secretAgreement.connect(signer2).agreeToSecret(secretHash);
-    
-    // Party 2 reveals the secret
-    const signature = await signMessage(secretHash, signer2);
-    await secretAgreement.connect(signer2).revealSecret(secretHash, "Secret Message", signature);
-  
-    // Check if the secret has been revealed and agreement is deleted
-    const agreement = await secretAgreement.connect(signer1).agreements(secretHash);
-    
-    //expect(agreement.isSecretRevealed).to.be.true;
-    expect(agreement.party1).to.equal("0x0000000000000000000000000000000000000000");
-    //find the logs of the event for the secret revealed
-    const agreementRevealedEvent = await secretAgreement.queryFilter(
-      secretAgreement.filters.SecretRevealed()
-    );
-    expect(agreementRevealedEvent[0].args.secret).to.equal("Secret Message");
-  });
-});
-
-it("should revert if the agreement already exists", async function () {
-
-  //Create unique secret hash
-  const secretHash = ethers.keccak256(ethers.toUtf8Bytes("BLAHHHHHH"));
-
-  // Party 1 creates the agreement
-  await secretAgreement.connect(signer3).createAgreement(secretHash, signer2.address);
-
-  // Party 1 tries to create the agreement again
-  await expect(secretAgreement.connect(signer3).createAgreement(secretHash, signer2.address)).to.be.revertedWithCustomError;
-
-});
-
-  it("should prevent an unauthorized party from revealing the secret", async function () {
-    const secretHash = ethers.keccak256(ethers.toUtf8Bytes("Secret Message"));
-
-    // Party 1 creates the agreement
-    await secretAgreement.createAgreement(secretHash, signer2.address);
-
-    // Unauthorized party tries to reveal the secret
-    const signature = await signMessage(secretHash, signer1);
-    await expect(secretAgreement.connect(signer3).revealSecret(secretHash,"bye friend", signature)).to.be.revertedWithCustomError;
-  });
-
-
-    it("Should agree to secret", async function () {
+    it("Should return undefined for party1 value when passed an unregistered hash", async function () {
       const secretHash = ethers.keccak256(ethers.toUtf8Bytes("Secret Message"));
-  
+      expect(await secretAgreement.agreements(secretHash).party1).to.equal(undefined);
+    });
+  });
+
+  describe("Create Agreement", function () {
+    it("should allow two parties to create an agreement and reveal the secret", async function () {
+      const secretHash = ethers.keccak256(ethers.toUtf8Bytes("Secret Message"));
+      
       // Party 1 creates the agreement
       await secretAgreement.connect(signer1).createAgreement(secretHash, signer2.address);
-  
-      // Party 2 agrees to the secret
+
+      // Party 1 & 2 agree to the secret
       await secretAgreement.connect(signer1).agreeToSecret(secretHash);
       await secretAgreement.connect(signer2).agreeToSecret(secretHash);
-  
-      // Check if the agreement has been updated
+
+      // Party 2 reveals the secret
+      const signature = await signMessage(secretHash, signer2);
+      await secretAgreement.connect(signer2).revealSecret(secretHash, "Secret Message", signature);
+
+      // Check if the secret has been revealed and agreement is deleted
+      const agreement = await secretAgreement.connect(signer1).agreements(secretHash);
+
+      // expect(agreement.isSecretRevealed).to.be.true;
+      expect(agreement.party1).to.equal("0x0000000000000000000000000000000000000000");
+
+      // Find the logs of the event for the secret revealed
+      const agreementRevealedEvent = await secretAgreement.queryFilter(secretAgreement.filters.SecretRevealed());
+      expect(agreementRevealedEvent[0].args.secret).to.equal("Secret Message");
+    });
+
+    it("should revert if the agreement already exists", async function () {
+      // Create unique secret hash
+      const secretHash = ethers.keccak256(ethers.toUtf8Bytes("BLAHHHHHH"));
+
+      // Party 1 creates the agreement
+      await secretAgreement.connect(signer3).createAgreement(secretHash, signer2.address);
+
+      // Party 1 tries to create the agreement again
+      await expect(secretAgreement.connect(signer3).createAgreement(secretHash, signer2.address)).to.be.revertedWithCustomError;
+    });
+
+    it("should check party1 is correct", async function () {
+      const secretHash = ethers.keccak256(ethers.toUtf8Bytes("Secret Message"));
+      await secretAgreement.connect(signer1).createAgreement(secretHash, signer2.address);
+      const agreement = await secretAgreement.agreements(secretHash);
+      expect(agreement.party1).to.equal(signer1.address);
+    });
+
+    it("should check party2 is correct", async function () {
+      const secretHash = ethers.keccak256(ethers.toUtf8Bytes("Secret Message"));
+      await secretAgreement.connect(signer1).createAgreement(secretHash, signer2.address);
       const agreement = await secretAgreement.agreements(secretHash);
       expect(agreement.party2).to.equal(signer2.address);
+    });
+
+    it("should check isParty1Voted is false", async function () {
+      const secretHash = ethers.keccak256(ethers.toUtf8Bytes("Secret Message"));
+      await secretAgreement.connect(signer1).createAgreement(secretHash, signer2.address);
+      const agreement = await secretAgreement.agreements(secretHash);
+      expect(agreement.isParty1Voted).to.be.false;
+    });
+
+    it("should check isParty2Voted is false", async function () {
+      const secretHash = ethers.keccak256(ethers.toUtf8Bytes("Secret Message"));
+      await secretAgreement.connect(signer1).createAgreement(secretHash, signer2.address);
+      const agreement = await secretAgreement.agreements(secretHash);
+      expect(agreement.isParty2Voted).to.be.false;
+    });
+  });
+
+  describe("Agree to Secret", function () {
+    it("Should agree party1 to secret", async function () {
+      const secretHash = ethers.keccak256(ethers.toUtf8Bytes("Secret Message"));
+
+      // Party 1 creates the agreement
+      await secretAgreement.connect(signer1).createAgreement(secretHash, signer2.address);
+
+      // Party 2 agrees to the secret
+      await secretAgreement.connect(signer1).agreeToSecret(secretHash);
+
+      // Check if the agreement has been updated
+      const agreement = await secretAgreement.agreements(secretHash);
       expect(agreement.party1).to.equal(signer1.address);
       expect(agreement.isParty1Voted).to.be.true;
     });
+
+    it("Should agree party2 to secret", async function () {
+      const secretHash = ethers.keccak256(ethers.toUtf8Bytes("Secret Message"));
+
+      // Party 1 creates the agreement
+      await secretAgreement.connect(signer1).createAgreement(secretHash, signer2.address);
+
+      // Party 2 agrees to the secret
+      await secretAgreement.connect(signer2).agreeToSecret(secretHash);
+
+      // Check if the agreement has been updated
+      const agreement = await secretAgreement.agreements(secretHash);
+      expect(agreement.party2).to.equal(signer2.address);
+      expect(agreement.isParty2Voted).to.be.true;
+    });
+
+    it("Should revert if the agreement does not exist", async function () {
+      const secretHash = ethers.keccak256(ethers.toUtf8Bytes("Secret Message"));
+      const wrongHash = ethers.keccak256(ethers.toUtf8Bytes("Wrong Hash"));
+      await secretAgreement.connect(signer1).createAgreement(secretHash, signer2.address);
+      await expect(secretAgreement.connect(signer1).agreeToSecret(wrongHash)).to.be.revertedWithCustomError;
+    });
+
+    it("Should revert if the party has already agreed to the secret", async function () {
+      const secretHash = ethers.keccak256(ethers.toUtf8Bytes("Secret Message"));
+      await secretAgreement.connect(signer1).createAgreement(secretHash, signer2.address);
+      await secretAgreement.connect(signer1).agreeToSecret(secretHash);
+      await expect(secretAgreement.connect(signer1).agreeToSecret(secretHash)).to.be.revertedWithCustomError;
+    });
+
+    it("Should revert if the party is not part of the agreement", async function () {
+      const secretHash = ethers.keccak256(ethers.toUtf8Bytes("Secret Message"));
+      await secretAgreement.connect(signer1).createAgreement(secretHash, signer2.address);
+      await expect(secretAgreement.connect(signer3).agreeToSecret(secretHash)).to.be.revertedWithCustomError;
+    });
+  });
+
+  describe("Reveal Secret", function () {
+    it("should prevent an unauthorized party from revealing the secret", async function () {
+      const secretHash = ethers.keccak256(ethers.toUtf8Bytes("Secret Message"));
+
+      // Party 1 creates the agreement
+      await secretAgreement.createAgreement(secretHash, signer2.address);
+
+      // Unauthorized party tries to reveal the secret
+      const signature = await signMessage(secretHash, signer1);
+      await expect(secretAgreement.connect(signer3).revealSecret(secretHash, "bye friend", signature)).to.be.revertedWithCustomError;
+    });
+
+    it("Should revert if both parties have not agreed to the secret", async function () {
+      const secretHash = ethers.keccak256(ethers.toUtf8Bytes("Secret Message"));
+
+      // Party 1 creates the agreement
+      await secretAgreement.connect(signer1).createAgreement(secretHash, signer2.address);
+
+      // Party 2 tries to reveal the secret
+      const signature = await signMessage(secretHash, signer2);
+      await expect(secretAgreement.connect(signer2).revealSecret(secretHash, "Secret Message", signature)).to.be.revertedWithCustomError;
+    });
+
+    it("Should revert if the secret has already been revealed", async function () {
+      const secretHash = ethers.keccak256(ethers.toUtf8Bytes("Secret Message"));
+
+      // Party 1 creates the agreement
+      await secretAgreement.connect(signer1).createAgreement(secretHash, signer2.address);
+
+      // Party 1 & 2 agree to the secret
+      await secretAgreement.connect(signer1).agreeToSecret(secretHash);
+      await secretAgreement.connect(signer2).agreeToSecret(secretHash);
+
+      // Party 2 reveals the secret
+      const signature = await signMessage(secretHash, signer2);
+      await secretAgreement.connect(signer2).revealSecret(secretHash, "Secret Message", signature);
+
+      // Party 2 tries to reveal the secret again
+      await expect(secretAgreement.connect(signer2).revealSecret(secretHash, "Secret Message", signature)).to.be.revertedWithCustomError;
+    });
+
+    it("Should revert if the signature is invalid", async function () {
+      const secretHash = ethers.keccak256(ethers.toUtf8Bytes("Secret Message"));
+
+      // Party 1 creates the agreement
+      await secretAgreement.connect(signer1).createAgreement(secretHash, signer2.address);
+
+      // Party 1 & 2 agree to the secret
+      await secretAgreement.connect(signer1).agreeToSecret(secretHash);
+      await secretAgreement.connect(signer2).agreeToSecret(secretHash);
+
+      // Party 2 tries to reveal the secret with an invalid signature
+      const signature = await signMessage(secretHash, signer1);
+      await expect(secretAgreement.connect(signer2).revealSecret(secretHash, "Secret Message", signature)).to.be.revertedWithCustomError;
+    });
+
+    it("Should revert if the secret hash is invalid", async function () {
+      const secretHash = ethers.keccak256(ethers.toUtf8Bytes("Secret Message"));
+      const wrongHash = ethers.keccak256(ethers.toUtf8Bytes("Wrong Hash"));
+
+      // Party 1 creates the agreement
+      await secretAgreement.connect(signer1).createAgreement(secretHash, signer2.address);
+
+      // Party 1 & 2 agree to the secret
+      await secretAgreement.connect(signer1).agreeToSecret(secretHash);
+      await secretAgreement.connect(signer2).agreeToSecret(secretHash);
+
+      // Party 2 tries to reveal the secret with an invalid secret hash
+      const signature = await signMessage(secretHash, signer2);
+      await expect(secretAgreement.connect(signer2).revealSecret(wrongHash, "Secret Message", signature)).to.be.revertedWithCustomError;
+    });
+
+    it("should emit event after secret is revealed", async function () {
+      const secretHash = ethers.keccak256(ethers.toUtf8Bytes("Secret Message"));
+      // Party 1 creates the agreement
+      await secretAgreement.connect(signer1).createAgreement(secretHash, signer2.address);
+
+      // Party 1 & 2 agree to the secret
+      await secretAgreement.connect(signer1).agreeToSecret(secretHash);
+      await secretAgreement.connect(signer2).agreeToSecret(secretHash);
+
+      // Party 2 reveals the secret
+      const signature = await signMessage(secretHash, signer2);
+      await secretAgreement.connect(signer2).revealSecret(secretHash, "Secret Message", signature);
+
+      // Check if the secret has been revealed and agreement is deleted
+      const agreement = await secretAgreement.connect(signer1).agreements(secretHash);
+      
+      // Find the logs of the event for the secret revealed
+      const agreementRevealedEvent = await secretAgreement.queryFilter(secretAgreement.filters.SecretRevealed());
+      expect(agreementRevealedEvent[0].args.secret).to.equal("Secret Message");
+    });
+  });
 
   async function signMessage(message, signer) {
     const messageBytes = ethers.getBytes(message);
@@ -90,3 +243,24 @@ it("should revert if the agreement already exists", async function () {
     return signature;
   }
 });
+
+
+
+//test deployement
+//check for contract created
+//check for owner or deployer
+//reveal secret function
+//write test where one part votes one doesnt and call reveal secret
+//test an invalid signature
+//test an invalid secret hash
+//test that event emitted shows revealer
+//create agreement should have a test that checks singer1 and signer2
+
+//Create design exericse with trade offs desribing design choices
+//create a diagram of the contract
+//Run gas report on contract
+//Run slither if necessary on contract
+
+
+
+
